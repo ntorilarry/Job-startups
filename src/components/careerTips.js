02b72d119f6aaab5a53d9224c8f09d7c4d420eff
Larry Ntori <l.ntori@voyager.local>
2022-06-08 11:26:48 +0000

    dev-larry

--- src/components/careerTips.js
+++ src/components/careerTips.js
@@ -12,7 +12,7 @@ function CareerTips() {
           className="absolute inset-x-0 top-0 h-48 bg-white"
         />
         <div className="relative">
-          <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-80">
+          <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-36">
             <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
               <div>
                 <div>
@@ -47,7 +47,7 @@ function CareerTips() {
           </div>
         </div>
         <div className="mt-24">
-          <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-80">
+          <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-36">
             <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
               <div>
                 <div className="mt-6">
